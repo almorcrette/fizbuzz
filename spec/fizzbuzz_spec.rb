80477b6fefd6032d78088842d5df45c1d054d49c
Joe McNally <joe.t.mcnally@hotmail.com>
2022-03-14 16:49:55 +0000

    extra test cases added

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when number is divisible by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  
+  it 'returns 4 when number is 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+  
+  it 'returns buzz when number is divisible by 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  
+  it 'returns fizzbuzz when number is divisible by both 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
